00cf20b741549a0372b743f02f4bcc5dabbf266d
Eugene Sadovoi <evgeni@eniks.com>
2018-01-19 17:46:42 -0500

    Init

---
+++ src/Benchmarks/UnaryOperators.cs
@@ -0,0 +1,24 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Attributes.Jobs;
+
+namespace CSharp.Benchmarks
+{
+    [ClrJob, CoreJob]
+    [BenchmarkCategory("Unary Operators")]
+    public class UnaryOperators
+    {
+        private int _value;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+        }
+
+        [Benchmark(Description = "i++")]
+        public int UnaryIncrement() => _value++;
+
+
+        [Benchmark(Description = "i--")]
+        public int UnaryDecrement() => _value++;
+    }
+}
---
+++ src/Program.cs
@@ -0,0 +1,18 @@
+﻿using BenchmarkDotNet.Running;
+using System.Reflection;
+
+namespace CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            if (0 == args.Length)
+
+                BenchmarkSwitcher.FromAssembly(typeof(Program).GetTypeInfo().Assembly).RunAllJoined();
+            else
+                BenchmarkSwitcher.FromAssembly(typeof(Program).GetTypeInfo().Assembly).Run(args);
+        }
+    }
+}
